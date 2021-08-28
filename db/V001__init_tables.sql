e8c4b1953e14d06fed7ad211bd3e18fcff6b605a
southheat92 <southheat92@gmail.com>
2021-08-28 13:15:14 -0400

    create table scripts

---
+++ db/V001__init_tables.sql
@@ -0,0 +1,33 @@
+CREATE TABLE leagues (
+  league_id SERIAL PRIMARY KEY,
+  name CHAR(100) NOT NULL
+);
+
+CREATE TABLE seasons (
+  season_id bigint PRIMARY KEY,
+  league_id int NOT NULL,
+  year int NOT NULL,
+  CONSTRAINT fk_league
+    FOREIGN KEY(league_id)
+      REFERENCES leagues(league_id)
+);
+
+CREATE TABLE owners (
+  owner_id bigint PRIMARY KEY,
+  display_name varchar(50) NOT NULL
+);
+
+CREATE TABLE teams (
+  season_id bigint NOT NULL,
+  owner_id bigint NOT NULL,
+  wins int NOT NULL,
+  losses int NOT NULL,
+  ties int NOT NULL,
+  PRIMARY KEY (season_id, owner_id),
+  CONSTRAINT fk_season
+    FOREIGN KEY(season_id) 
+	    REFERENCES seasons(season_id),
+  CONSTRAINT fk_owner
+    FOREIGN KEY (owner_id)
+      REFERENCES owners(owner_id)
+);
