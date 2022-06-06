6a4aec57c0a1f55d285a3100a0488ccb7b477794
Alex Mann <mann.alext@gmail.com>
2022-06-06 14:28:41 -0400

    swagger docs almost work

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@
 import Koa from 'koa';
 import KoaRouter from '@koa/router';
 import { RegisterRoutes } from './routes';
+import { koaSwagger } from 'koa2-swagger-ui';
+import KoaMount from 'koa-mount';
+import KoaServe from 'koa-static';
+
 
 export class App {
   // async createLeague(leagueName: string): Promise<League> {
@@ -11,15 +15,36 @@ export class App {
   
   run(): void {
     const app: Koa = new Koa();
+    this.enableSwaggerDocs(app);
     const router = new KoaRouter();
     RegisterRoutes(router);
     app.listen(8080);
     console.log('anybody out there?');
 
+    app.use(router.routes()).use(router.allowedMethods());
+    
 
-    // app.use(async (context: Koa.Context) => {
-    //   context.body = 'hello world';
-    // });
+    app.use(async (context: Koa.Context) => {
+      context.body = 'hello world';
+    });
 
   }
+
+  private enableSwaggerDocs(app: Koa) {
+    app.use(koaSwagger({ 
+        routePrefix: "docs", 
+        swaggerOptions: { 
+            url: 'spec',
+        },
+        hideTopbar: true, 
+        title: 'FantasyStats API',
+        favicon: 'https://www.physna.com/assets/images/icon/favicon-32x32.png',
+        // oauthOptions: { 
+        //     usePkceWithAuthorizationCodeGrant: true,
+        //     clientId: process.env.oktaOauthClientId,
+        //     scopes: ["tenant"]
+        // },
+    }));
+    app.use(KoaMount('/v2', KoaServe('./node_modules/swagger-ui-dist')));
+}
 }
