fc870bdb1def2d8e088f07fc9de99874235f54ad
Alex Mann <mann.alext@gmail.com>
2022-06-06 20:28:17 -0400

    remove swagger docs temporarily, get api to work

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,9 @@
 import Koa from 'koa';
 import KoaRouter from '@koa/router';
 import { RegisterRoutes } from './routes';
-import { koaSwagger } from 'koa2-swagger-ui';
-import KoaMount from 'koa-mount';
-import KoaServe from 'koa-static';
+// import { koaSwagger } from 'koa2-swagger-ui';
+// import KoaMount from 'koa-mount';
+// import KoaServe from 'koa-static';
 
 
 export class App {
@@ -15,36 +15,39 @@ export class App {
   
   run(): void {
     const app: Koa = new Koa();
-    this.enableSwaggerDocs(app);
     const router = new KoaRouter();
     RegisterRoutes(router);
-    app.listen(8080);
-    console.log('anybody out there?');
-
+    app.listen(9090);
     app.use(router.routes()).use(router.allowedMethods());
+    console.log('anybody out there?');
+    
+    // this.enableSwaggerDocs(app);
     
 
-    app.use(async (context: Koa.Context) => {
-      context.body = 'hello world';
-    });
+    // app.use(async (context: Koa.Context) => {
+    //   context.body = 'hello world';
+    // });
 
   }
 
-  private enableSwaggerDocs(app: Koa) {
-    app.use(koaSwagger({ 
-        routePrefix: "docs", 
-        swaggerOptions: { 
-            url: 'spec',
-        },
-        hideTopbar: true, 
-        title: 'FantasyStats API',
-        favicon: 'https://www.physna.com/assets/images/icon/favicon-32x32.png',
-        // oauthOptions: { 
-        //     usePkceWithAuthorizationCodeGrant: true,
-        //     clientId: process.env.oktaOauthClientId,
-        //     scopes: ["tenant"]
-        // },
-    }));
-    app.use(KoaMount('/v2', KoaServe('./node_modules/swagger-ui-dist')));
+  // private enableSwaggerDocs(app: Koa) {
+  //   app.use(koaSwagger({ 
+  //       routePrefix: "docs", 
+  //       swaggerOptions: { 
+  //           url: 'spec',
+  //       },
+  //       hideTopbar: true, 
+  //       title: 'FantasyStats API',
+  //       favicon: 'https://www.physna.com/assets/images/icon/favicon-32x32.png',
+  //       // oauthOptions: { 
+  //       //     usePkceWithAuthorizationCodeGrant: true,
+  //       //     clientId: process.env.oktaOauthClientId,
+  //       //     scopes: ["tenant"]
+  //       // },
+  //   }));
+  //   app.use(KoaMount('/', KoaServe('./node_modules/swagger-ui-dist')));
+  // }
 }
-}
+
+const app = new App();
+app.run();
