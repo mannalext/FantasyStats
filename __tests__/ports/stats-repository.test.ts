42b45cd1a0ceb836910309a765b35df74960826b
Alex Mann <mann.alext@gmail.com>
2022-08-29 20:20:14 -0400

    createLeague tests

--- __tests__/ports/stats-repository.test.ts
+++ __tests__/ports/stats-repository.test.ts
@@ -1,4 +1,6 @@
+import { League } from '../../src/entities/league';
 import { StatsRepository } from '../../src/ports/stats/stats-repository';
+import { isNumber } from '../../src/utilities/is-number';
 import { getPortsForTesting } from '../helpers/ports-for-testing';
 
 describe('stats-repository', () => {
@@ -13,25 +15,29 @@ describe('stats-repository', () => {
   describe('createLeague', () => {
     describe('when a valid league name is given', () => {
       const leagueName = 'testLeague';
+      let leagueId: number;
 
-      it('creates a league', () => {
-        repo.createLeague(leagueName);
-        expect(true).toBe(true);
+      beforeAll(async () => {
+        leagueId = await repo.createLeague(leagueName);
       });
 
-      // it('returns the league id', () => {
+      it('returns the league id', () => {
+        expect(isNumber(leagueId)).toBe(true);
+      });
 
-      // });
+      it('creates a league', async () => {
+        const league: League = {
+          id: leagueId,
+          name: leagueName,
+        };
+        expect(await repo.findLeagueById(leagueId)).toEqual(league);
+      });
 
-      // it('increments league ids for successively created leagues', () => {
-
-      // });
+      it('increments league ids for successively created leagues', async () => {
+        const newLeagueId = await repo.createLeague('newLeague)');
+        const anotherNewLeagueId = await repo.createLeague('anotherNewLeague');
+        expect(anotherNewLeagueId).toEqual(newLeagueId + 1);
+      });
     });
-
-    // describe('when an invalid league name is given', () => {
-    //   it('fails gracefully', () => { // TODO: refine this statement
-
-    //   });
-    // });
   });
 });
