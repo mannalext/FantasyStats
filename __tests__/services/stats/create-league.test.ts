07a6cc590d2eda5e62914d908b30c02808b1ddfe
Alex Mann <mann.alext@gmail.com>
2022-08-29 21:19:31 -0400

    service level tests for create-league and find-league-by-id

---
+++ __tests__/services/stats/create-league.test.ts
@@ -0,0 +1,18 @@
+import { createLeague } from '../../../src/services/stats/create-league';
+import { findLeagueById } from '../../../src/services/stats/find-league-by-id';
+
+describe('createLeague service', () => {
+  describe('when a potential league name is passed in', () => {
+    it('creates a league', async () => {
+      const someLeagueName = 'someLeagueName';
+      const leagueId = await createLeague(someLeagueName);
+
+      const league = await findLeagueById(leagueId);
+
+      expect(league).toEqual({
+        name: someLeagueName,
+        id: leagueId,
+      });
+    });
+  });
+});
