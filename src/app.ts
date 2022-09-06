c6151936757b4c8d7bb86eb3d45ecca5bdf69245
Alex Mann <mann.alext@gmail.com>
2022-09-06 14:02:36 -0400

    re-add swagger ui

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,12 @@ import Koa from 'koa';
 import KoaRouter from '@koa/router';
 import bodyParser from 'koa-bodyparser';
 import { RegisterRoutes } from './routes';
-// import { koaSwagger } from 'koa2-swagger-ui';
-// import KoaMount from 'koa-mount';
-// import KoaServe from 'koa-static';
+import { koaSwagger } from 'koa2-swagger-ui';
+import KoaMount from 'koa-mount';
+import KoaServe from 'koa-static';
+import * as fs from 'node:fs';
 
 export class App {
-  // async createLeague(leagueName: string): Promise<League> {
-  //   const repo = new PgStatsRepository();
-  //   return await repo.createLeague(leagueName);
-  // }
-
   run(): void {
     const app: Koa = new Koa();
     app.use(bodyParser());
@@ -22,30 +18,24 @@ export class App {
     app.use(router.routes()).use(router.allowedMethods());
     console.log(`anybody out there? port: ${port}`);
 
-    // this.enableSwaggerDocs(app);
-
-    // app.use(async (context: Koa.Context) => {
-    //   context.body = 'hello world';
-    // });
+    this.enableSwaggerDocs(app);
   }
 
-  // private enableSwaggerDocs(app: Koa) {
-  //   app.use(koaSwagger({
-  //       routePrefix: "docs",
-  //       swaggerOptions: {
-  //           url: 'spec',
-  //       },
-  //       hideTopbar: true,
-  //       title: 'FantasyStats API',
-  //       favicon: 'https://www.physna.com/assets/images/icon/favicon-32x32.png',
-  //       // oauthOptions: {
-  //       //     usePkceWithAuthorizationCodeGrant: true,
-  //       //     clientId: process.env.oktaOauthClientId,
-  //       //     scopes: ["tenant"]
-  //       // },
-  //   }));
-  //   app.use(KoaMount('/', KoaServe('./node_modules/swagger-ui-dist')));
-  // }
+  private enableSwaggerDocs(app: Koa) {
+    const spec = fs.readFileSync('swagger.json').toString();
+
+    app.use(
+      koaSwagger({
+        routePrefix: '/swagger',
+        swaggerOptions: {
+          spec: JSON.parse(spec),
+        },
+        hideTopbar: true,
+        title: 'FantasyStats API',
+      })
+    );
+    app.use(KoaMount('/', KoaServe('./node_modules/swagger-ui-dist')));
+  }
 }
 
 const app = new App();
