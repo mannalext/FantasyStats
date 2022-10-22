c27435822fdddeb9c946627112e1a224795b93cd
mannalext <mann.alext@gmail.com>
2022-10-22 17:37:36 +0000

    add supertest and test file with notes

---
+++ __tests__/integration/league-controller.spec.ts
@@ -0,0 +1,15 @@
+describe('league-controller', () => {
+  /**
+   * TODO:
+   * 1. import app from app.ts
+   * 2. feed it to supertest?
+   * 3. invoke a get on that supertest object to see if it hits my route
+   * 4. observe the response
+   *
+   * also:
+   * - figure out how this is going to play with the real/fake ports infrastructure
+   * - how to make these idempotent?
+   * - should they be run separately from the unit tests? (probably)
+   * - jest configuration to make all the above happen?
+   */
+});
