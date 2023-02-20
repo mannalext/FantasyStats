fd12b3a2779e7e71f4447dbd96c953b004ccb45a
mannalext <>
2023-02-20 20:09:17 +0000

    finish leagues-controller api test implementation

--- __tests__/integration/league-controller.spec.ts
+++ __tests__/integration/league-controller.spec.ts
@@ -3,30 +3,24 @@ import axios from 'axios';
 const host = process.env['STATS_API_URL'];
 
 describe('league-controller', () => {
-  /**
-   * TODO:
-   * 1. import app from app.ts
-   * 2. feed it to supertest?
-   * 3. invoke a get on that supertest object to see if it hits my route
-   * 4. observe the response
-   *
-   * also:
-   * - figure out how this is going to play with the real/fake ports infrastructure
-   * - how to make these idempotent?
-   * - should they be run separately from the unit tests? (probably)
-   * - jest configuration to make all the above happen?
-   */
+  it('creating and fetching a league', async () => {
+    const leagueName = 'IntegrationTestLeague';
 
-  it('does a thing', async () => {
-    const response = await axios.get(`${host}leagues/48`); // TODO: parameterize
-    // TODO: host can be an env. for replit, a secret
+    const createLeagueResponse = await axios.post(`${host}leagues`, {
+      leagueName,
+    });
+    const leagueId = createLeagueResponse.data;
 
-    // TODO: idempotency
+    const response = await axios.get(`${host}leagues/${leagueId}`);
+    // TODO: host can be an env. for replit, a secre
 
     // TODO: script for non replit needs to start and stop the app
 
-    console.log(response.data);
-
-    expect(response.data).toEqual(123);
+    expect(response.data).toEqual({
+      league: {
+        name: leagueName,
+        id: leagueId,
+      },
+    });
   });
 });
