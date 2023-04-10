7121f53d24d8133490bf03da3d9b3d512500a05d
Alex Mann <mann.alext@gmail.com>
2023-04-10 10:39:39 -0400

    feat: season-controller implementation

--- src/api/season-controller.ts
+++ src/api/season-controller.ts
@@ -1,6 +1,9 @@
 import { Season } from '@entities/season';
-import { Controller, Get, Path, Res, Route, TsoaResponse } from 'tsoa';
+import { Body, Controller, Get, Path, Post, Res, Route, TsoaResponse } from 'tsoa';
 import { ErrorResponse } from './error-message';
+import { findSeasonById } from '@services/stats/seasons/find-season-by-id';
+import { findSeasonByLeagueAndYear } from '@services/stats/seasons/find-season-by-league-and-year';
+import { createSeason } from '@services/stats/seasons/create-season';
 
 interface SingleSeasonResponse {
   season: Season | undefined;
@@ -13,6 +16,40 @@ export class SeasonsController extends Controller {
     @Path() id: number,
     @Res() notFoundResponse: TsoaResponse<404, ErrorResponse>
   ): Promise<SingleSeasonResponse> {
-    return {} as unknown as SingleSeasonResponse;
+    const found: Season | undefined = await findSeasonById(id);
+    if (!found) {
+      return notFoundResponse(404, {
+        error: {
+          message: 'No season found for that id',
+        },
+      });
+    }
+    return {
+      season: found,
+    };
+  }
+
+  @Get('{leagueId}')
+  public async findSeasonByLeagueAndYear(
+    @Path() leagueId: number,
+    @Path() year: number,
+    @Res() notFoundResponse: TsoaResponse<404, ErrorResponse>
+  ): Promise<SingleSeasonResponse> {
+    const found: Season | undefined = await findSeasonByLeagueAndYear(leagueId, year);
+    if (!found) {
+      return notFoundResponse(404, {
+        error: {
+          message: 'No season found for that league and year',
+        },
+      });
+    }
+    return {
+      season: found,
+    };
+  }
+
+  @Post('')
+  public async createSeason(@Body() body: { leagueId: number }): Promise<number> {
+    return await createSeason(body.leagueId);
   }
 }
