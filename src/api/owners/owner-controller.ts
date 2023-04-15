b69bd729758c4abcf91ba081e0ad5fe189f555e4
Alex Mann <mann.alext@gmail.com>
2023-04-15 11:58:07 -0400

    create owner error handling middleware

--- src/api/owners/owner-controller.ts
+++ src/api/owners/owner-controller.ts
@@ -1,14 +1,16 @@
-import { Owner } from '../entities/owner';
-import { findOwnerById } from '../services/stats/owners/find-owner-by-id';
-import { Route, Controller, Get, Path, Post, Res, TsoaResponse, Body } from 'tsoa';
-import { ErrorResponse } from './error-message';
-import { createOwner } from '../services/stats/owners/create-owner';
+import { Owner } from '../../entities/owner';
+import { findOwnerById } from '../../services/stats/owners/find-owner-by-id';
+import { Route, Controller, Get, Path, Post, Res, TsoaResponse, Body, Middlewares } from 'tsoa';
+import { ErrorResponse } from '../error-message';
+import { createOwner } from '../../services/stats/owners/create-owner';
+import { OwnerErrorHandlingMiddleware } from './owner-error-handling-middleware';
 
 interface SingleOwnerResponse {
   owner: Owner | undefined;
 }
 
 @Route('owners')
+@Middlewares(OwnerErrorHandlingMiddleware)
 export class OwnersController extends Controller {
   @Get('{id}')
   public async findOwnerById(
