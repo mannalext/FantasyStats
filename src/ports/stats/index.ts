c29d9fdbaaa154fd8ca563ab4d988b89e5d26a15
Alex Mann <mann.alext@gmail.com>
2023-04-16 12:39:36 -0400

    global prisma instance

--- src/ports/stats/index.ts
+++ src/ports/stats/index.ts
@@ -2,6 +2,7 @@ import { Pool, QueryResult } from 'pg';
 import { Configuration } from '../../configuration';
 import { PrismaClient } from '@prisma/client';
 
+// TODO: remove this old db config once prisma is fully implemented
 const databaseConfig = new Configuration().getDatabaseConfig();
 
 const pool = new Pool(databaseConfig);
@@ -9,4 +10,14 @@ const pool = new Pool(databaseConfig);
 export const query = async (query: string, parameters?: unknown[]): Promise<QueryResult> =>
   await pool.query(query, parameters);
 
-export const prisma = new PrismaClient();
+interface PrismaGlobal {
+  prisma: PrismaClient;
+}
+
+declare const global: PrismaGlobal;
+
+const prisma = global.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+
+export default prisma;
