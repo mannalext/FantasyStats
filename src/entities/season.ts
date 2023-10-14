f3806abf7cacca2344f18e879f55aa27043768f6
Alex Mann <mann.alext@gmail.com>
2023-10-14 18:29:58 -0400

    DRY up season.ts

--- src/entities/season.ts
+++ src/entities/season.ts
@@ -11,17 +11,11 @@ export class Season {
 }
 
 export class SleeperSeason extends Season {
-  id: number;
   sleeperLeagueId: string;
-  leagueId: number;
-  year: number;
 
   constructor(id: number, sleeperLeagueId: string, leagueId: number, year: number) {
     super(id, leagueId, year);
-    this.id = id;
     this.sleeperLeagueId = sleeperLeagueId;
-    this.leagueId = leagueId;
-    this.year = year;
   }
 }
 
